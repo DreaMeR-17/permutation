fe8c044b8a0b2881e6e3797b7934df9ade56ec57
DreaMeR <DreaMeR@DREAMER>
2024-07-09 10:49:05 +0600

    Добавьте файлы проекта.

---
+++ permutation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace permutation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = ("Nathan");
+            string secondName = ("Feuerstein");
+
+            Console.WriteLine(firstName + " " + secondName);
+
+            (firstName, secondName) = (secondName, firstName);
+
+            Console.WriteLine(firstName + " " + secondName);
+        }
+    }
+}
